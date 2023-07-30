6553f398e45d9eb9577dac7ef8eb2a9bda134bbb
imeadbullah <abdullahprime98@gmail.com>
2023-07-30 17:36:07 +0600

    recursion basics

---
+++ recursion/main.cpp
@@ -0,0 +1,45 @@
+#include<bits/stdc++.h>
+using namespace std;
+
+void printName(int i, int n) {
+    if(i > n) {
+        return;
+    }
+    cout << "Abdullah" << endl;
+    printName(i+1, n);
+}
+
+// parameterized vs funcitonal
+void sumNum(int i,int n, int sum) {
+    if(i > n) {
+        cout << sum << endl;
+        return;
+    }
+    sumNum(i+1,n, sum+i);
+}
+
+int sum(int i) {
+    if(i <= 1) {
+        return i;
+    }
+    return i + sum(i-1);
+}
+
+void arrSwap(int i, int n, int arr[]) {
+    if(i > (n/2)) {
+        return;
+    }
+    swap(arr[i], arr[n-i-1]);
+    arrSwap(i+1,n, arr);
+}
+
+int main() {
+    //printName(0, 10);
+    //sumNum(0, 4, 0);
+    //cout << sum(4);
+    int arr[] = {1,2,3,4,5};
+    arrSwap(0,5,arr);
+    for(int i = 0;i<5;i++) {
+        cout << arr[i] << " ";
+    }
+}
